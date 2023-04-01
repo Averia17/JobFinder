f78e0b23b27d448c6c2f0735adc87611089f18f0
katya <katiamotolianec@gmail.com>
2023-04-01 14:45:30 +0300

    routes refactoring

--- frontend/src/routes/Router.js
+++ frontend/src/routes/Router.js
@@ -1,6 +1,6 @@
 import {BrowserRouter} from "react-router-dom";
 import {Route, Routes} from "react-router";
-import {authorizedUserRoutes, employeeRoutes, unauthorizedUserRoutes} from "./router.config";
+import {authorizedUserRoutes, commonAuthorizedRoutes, employeeRoutes, unauthorizedUserRoutes} from "./router.config";
 import {useGetInfoFromToken} from "../hooks/useGetInfoFromToken/useGetInfoFromToken";
 import Header from "../components/header/Header";
 import CompanyPage from "../pages/company/CompanyPage";
@@ -8,6 +8,7 @@ import VacanciesPage from "../pages/vacancies/VacanciesPage";
 import VacancyPage from "../pages/vacancy/VacancyPage";
 import ResumesPage from "../pages/resumes/ResumesPage";
 import MainPage from "../pages/MainPage";
+import {mapRoutes} from "./router.service";
 
 const Router = () => {
     const tokenInfo = useGetInfoFromToken();
@@ -21,42 +22,10 @@ const Router = () => {
                     <Route path='/vacancies/:id' element={<VacancyPage/>}/>
                     <Route path='/resumes' element={<ResumesPage/>}/>
                     <Route path='/' element={<MainPage/>}/>
-                    {
-                        !tokenInfo?.user_id ?
-                            Object.keys(unauthorizedUserRoutes).map((key) => {
-                                return (
-                                    <Route
-                                        key={key}
-                                        path={key}
-                                        element={unauthorizedUserRoutes[key].element}
-                                    />
-                                );
-                            }) : null
-                    }
-                    {
-                        tokenInfo?.user_id && (!tokenInfo?.company) ?
-                            Object.keys(authorizedUserRoutes).map((key) => {
-                                return (
-                                    <Route
-                                        key={key}
-                                        path={key}
-                                        element={authorizedUserRoutes[key].element}
-                                    />
-                                );
-                            }) : null
-                    }
-                    {
-                        tokenInfo?.company ?
-                            Object.keys(employeeRoutes).map((key) => {
-                                return (
-                                    <Route
-                                        key={key}
-                                        path={key}
-                                        element={employeeRoutes[key].element}
-                                    />
-                                );
-                            }) : null
-                    }
+                    { tokenInfo?.user_id ? mapRoutes(commonAuthorizedRoutes): null }
+                    { !tokenInfo?.user_id ? mapRoutes(unauthorizedUserRoutes) : null }
+                    { tokenInfo?.user_id && (!tokenInfo?.company) ? mapRoutes(authorizedUserRoutes) : null }
+                    { tokenInfo?.company ? mapRoutes(employeeRoutes): null }
                 </Routes>
             </BrowserRouter>
         </>
