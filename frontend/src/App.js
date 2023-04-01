b8a5af31ba341e03edb22620df1de7d0845ee059
katya <katiamotolianec@gmail.com>
2023-04-01 14:13:00 +0300

    refactor imports in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,5 @@
 import axios from "axios";
-import LoginPage from "./pages/LoginPage";
-import RegisterPage from "./pages/RegisterPage";
-import MainPage from "./pages/MainPage";
-
 import './App.css';
-import Header from "./components/header/Header";
-import CompanyPage from "./pages/company/CompanyPage";
-import VacanciesPage from "./pages/vacancies/VacanciesPage";
-import VacancyPage from "./pages/vacancy/VacancyPage";
-import ResumesPage from "./pages/resumes/ResumesPage";
-import ResumeForm from "./components/resume-form/ResumeForm";
-import ResponsesPage from "./pages/responses/ResponsesPage";
-import ProfilePage from "./pages/profile/ProfilePage";
 import Router from "./routes/Router";
 
 axios.defaults.baseURL = 'http://localhost:8000'
