87aee93251d909bd53cb9024f80d66a4af6a2151
unknown <feterfox@gmail.com>
2023-04-11 00:06:47 +0300

    one request for favorites

--- job_finder/favorites/views.py
+++ job_finder/favorites/views.py
@@ -1,5 +1,7 @@
 from rest_framework.mixins import CreateModelMixin, ListModelMixin
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.status import HTTP_200_OK
 from rest_framework.viewsets import GenericViewSet
 
 from favorites.models import FavoriteVacancies
@@ -13,3 +15,10 @@ class FavoriteVacanciesViewSet(CreateModelMixin, ListModelMixin, GenericViewSet)
 
     def get_queryset(self):
         return super().get_queryset().filter(user=self.request.user)
+
+    def create(self, request, *args, **kwargs):
+        favorites = FavoriteVacancies.objects.filter(user=request.user, vacancy=request.data["vacancy"])
+        if favorites.exists():
+            favorites.delete()
+            return Response("Successfully removed", status=HTTP_200_OK)
+        return super().create(request, *args, **kwargs)
