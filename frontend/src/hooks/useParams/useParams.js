c680009c3a3d704a73e3184448b7629ab4372815
katya <katiamotolianec@gmail.com>
2023-05-31 22:59:59 +0300

    add all styles to vacancy page

---
+++ frontend/src/hooks/useParams/useParams.js
@@ -0,0 +1,51 @@
+import {useSearchParams} from "react-router-dom";
+import {useEffect, useMemo, useState} from "react";
+import {useNavigate} from "react-router";
+
+const paramsNames = {
+    responseId: 'responseId'
+}
+
+export const useParams = () => {
+    const navigate = useNavigate();
+    const [queryParams] = useSearchParams();
+
+    const params = useMemo(() => {
+        let responseId = queryParams.get(paramsNames.responseId) || undefined;
+
+        return {
+            responseId: responseId | undefined,
+        };
+    }, [queryParams]);
+
+    const [state, setState] = useState(params);
+
+    const setParams = (newParams) => {
+        setState((prev) => ({ ...prev, ...newParams }));
+    };
+
+    useEffect(() => {
+        setState((prev) => ({ ...prev, ...params }));
+    }, [params]);
+
+    useEffect(() => {
+        Object.values(paramsNames).forEach((key) => {
+            const value = state[key];
+
+            if (value) {
+                queryParams.set(key, value.toString());
+            } else {
+                queryParams.delete(key);
+            }
+        });
+        navigate({ search: queryParams.toString() });
+
+    }, [state]);
+
+
+    return {
+        ...state,
+        setParams,
+    };
+
+}
--- frontend/src/pages/vacancy/VacancyPage.js
+++ frontend/src/pages/vacancy/VacancyPage.js
@@ -147,35 +147,31 @@ const VacancyPage = () => {
     return (
         <div className='vacancyPage__container'>
             <div className='vacancyPage__header__container'>
-                <div className='header__title'>
-                    <h1>{title}</h1>
-                    <h2><Link className="company__link" to={`/companies/${company?.id}`}>{company?.title}</Link></h2>
-                    <div>
-                        <img src={company} alt=""/>
+                <div className='header__title__container'>
+                    <div className='header__title'>
+                        <h1>{title}</h1>
+                        <h2><Link className="company__link" to={`/companies/${company?.id}`}>{company?.title}</Link></h2>
+                        {!vacancyInfo.is_active && <b>
+                            <div style={{color: "red"}}>Вакансия находится в архиве</div>
+                        </b>}
+                        <p className='vacancy__salary'>{renderSalaryIfExists(vacancyInfo.min_salary, vacancyInfo.max_salary)}</p>
+                        <div>{vacancyInfo.employment_type}</div>
+                        <div>Требуемый опыт работы: {vacancyInfo.experience_option?.includes('0') ? 'не требуется'
+                                : vacancyInfo.experience_option}
+                        </div>
+                        <p>Создана {vacancyInfo?.created && dayjs(vacancyInfo?.created).format("DD.MM.YYYY")}</p>
+                        {tokenInfo?.user_id && <div className='vacancyPage__header__buttons'>
+                            {!tokenInfo?.company && <Button onClick={respondToVacancy}
+                                                            disabled={vacancyInfo?.is_responded || !vacancyInfo?.is_active}>Откликнуться</Button>}
+                            <FavoriteButton onClick={handleClickChangeFavoriteStatus} is_favorite={isVacancyFavorite}/>
+                        </div>}
                     </div>
+                    {company?.image && <div className='company__logo'><img src={`http://localhost:8000/${company?.image}`} alt=""/></div>}
                 </div>
-                <div className='header__info'>
-
-                </div>
-
-                {!vacancyInfo.is_active && <b>
-                    <div style={{color: "red"}}>Вакансия находится в архиве</div>
-                </b>}
-                <div>{renderSalaryIfExists(vacancyInfo.min_salary, vacancyInfo.max_salary)}</div>
-                <p>Создана {vacancyInfo?.created && dayjs(vacancyInfo?.created).format("DD.MM.YYYY")}</p>
-                <div>{vacancyInfo.employment_type}</div>
-                <div>Обязательный опыт {vacancyInfo.experience_option}</div>
-                {tokenInfo?.user_id && <div className='vacancyPage__header__buttons'>
-                    {!tokenInfo?.company && <Button onClick={respondToVacancy}
-                                                    disabled={vacancyInfo?.is_responded || !vacancyInfo?.is_active}>Откликнуться</Button>}
-                        <FavoriteButton onClick={handleClickChangeFavoriteStatus} is_favorite={isVacancyFavorite}/>
-                </div>}
             </div>
             {vacancyInfo.description &&
                 <div>
-                    <hr/>
                     <div style={{whiteSpace: "pre-line"}}>{vacancyInfo.description}</div>
-                    <hr/>
                 </div>
             }
 
